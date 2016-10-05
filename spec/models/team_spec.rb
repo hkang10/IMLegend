6412cf7f96514d92f13b9642b00d131f18e0360d
Dan Dobrick & Mark McLoughlin <pair+dandobrick.mark-mcl@devbootcamp.com>
2016-10-05 16:31:51 -0500

    Write tests for team properties; passing all tests

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,1 +1,19 @@
+require 'rails_helper'
 
+describe Team do
+  let(:team) { Team.new(teacher_id: 1, captain_id: 12, team_name: 'Probably should have gone with name') }
+
+  describe "Has readable properties" do
+    it "Has a readable teacher_id" do
+      expect(team.teacher_id).to eq 1
+    end
+
+    it "Has a readable captain_id" do
+      expect(team.captain_id).to eq 12
+    end
+
+    it 'Has a readable name' do
+      expect(team.team_name).to eq 'Probably should have gone with name'
+    end
+  end
+end
