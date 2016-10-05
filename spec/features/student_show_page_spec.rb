edf1ceff99d1735bd30b8277e34f5fd42418daa0
Dan Dobrick & Mark McLoughlin <pair+dandobrick.mark-mcl@devbootcamp.com>
2016-10-05 15:31:45 -0500

    Add student migration

---
+++ spec/features/student_show_page_spec.rb
@@ -0,0 +1,40 @@
+require "rails_helper"
+
+feature "student's show page" do
+  describe "when teacher navigates to a student's show page" do
+
+    xscenario "the teacher sees the student's name" do
+    end
+
+    xscenario "the teacher sees the student's gpa" do
+    end
+
+    xscenario "the teacher sees the student's gender" do
+    end
+
+    xscenario "the teacher sees the student's number of detention" do
+    end
+
+    xscenario "the teacher sees the student's sports team" do
+    end
+
+    xscenario "the teacher sees the student's number of detention" do
+    end
+
+    describe "when the student is already on a team" do
+      xscenario "the teacher sees the student's team name" do
+      end
+    end
+
+    describe "when the student is available" do
+      xscenario "the teacher sees an 'add to my team' button" do
+      end
+    end
+
+    describe "when the student is already on the teacher's team" do
+      xscenario "the teacher sees a 'make new captain' button" do
+      end
+    end
+
+  end
+end
