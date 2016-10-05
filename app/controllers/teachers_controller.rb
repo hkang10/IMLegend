a26a3217aa6344e9d1e7f3329e36980394284fbe
Audrey Kang <email": null>
2016-10-05 18:46:24 -0500

    fix heroku

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -7,14 +7,14 @@ class TeachersController < ApplicationController
 
  def create
    @teacher = Teacher.new(teacher_params)
-   # @team = @teacher.team.create(params[:teacher][:team_name])
+   @team = @teacher.team.create(params[:teacher][:team_name])
 
    if @teacher.save
      session[:teacher_id] = @teacher.id
      if @teacher.admin? == true
         redirect_to teacher_path
       else
-        redirect_to teams_path
+        redirect_to team_path(@teamg)
       end
    else
      @errors = @teacher.errors.full_messages
