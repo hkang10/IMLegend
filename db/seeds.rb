b8a24feea671dec0c842571224f02e0a00700dab
Dan Dobrick & Mark McLoughlin <pair+dandobrick.mark-mcl@devbootcamp.com>
2016-10-06 10:35:57 -0500

    Finish student show with tests to prove it

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+teacher = Teacher.create!(first_name: 'Travis', last_name: 'CI', email: 'tmoney@ymail.com', password: 'password', admin?: false)
+team = Team.create!(teacher_id: teacher.id, captain_id: nil, team_name: 'Probably should have gone with name')
+Student.create!(first_name: 'Dan', last_name: 'D', gender: 'm', gpa: 2.5, number_of_detentions: 8, sports_teams: ['Football', 'Foosball', 'Sand Hockey'], number_of_absences: 99, team_id: team.id)
+student = Student.create!(first_name: 'Mark', last_name: 'M', gender: 'm', gpa: 3.95, number_of_detentions: 4, sports_teams: ['Football', 'Foosball', 'Sand Hockey'], number_of_absences: 23, team_id: team.id)
+Student.create!(first_name: 'Bob', last_name: 'D', gender: 'm', gpa: 1.2, number_of_detentions: 5, sports_teams: ['Football', 'Foosball', 'Sand Hockey'], number_of_absences: 14, team_id: nil)
+
+team.captain = student
+team.save
--- spec/features/student_show_page_spec.rb
+++ spec/features/student_show_page_spec.rb
@@ -107,17 +107,64 @@ feature "student's show page" do
     end
 
     describe "when the student is already on a team" do
-      xscenario "the teacher sees the student's team name" do
-      end
+      scenario "the teacher sees the student's team name" do
+        teacher = Teacher.create!(first_name: 'Travis', last_name: 'CI', email: 'tmoney@ymail.com', password: 'password', admin?: false)
+        student = Student.create!(first_name: 'Dan', last_name: 'D', gender: 'm', gpa: 2.5, number_of_detentions: 8, sports_teams: ['Football', 'Foosball', 'Sand Hockey'], number_of_absences: 99, team_id: nil)
+        team = Team.create!(teacher_id: teacher.id, captain_id: student.id, team_name: 'Probably should have gone with name')
+        student.team = team
+        student.save
+        visit root_path
+
+        within('.login-form') do
+          fill_in :email, with: 'tmoney@ymail.com'
+          fill_in :password, with: 'password'
+          click_button('Login')
+        end
+
+        visit student_path(student)
+        expect(page).to have_current_path student_path(student)
+        expect(page).to have_content student.team.team_name
+        end
     end
 
     describe "when the student is available" do
-      xscenario "the teacher sees an 'add to my team' button" do
+      scenario "the teacher sees an 'add to my team' button" do
+        teacher = Teacher.create!(first_name: 'Travis', last_name: 'CI', email: 'tmoney@ymail.com', password: 'password', admin?: false)
+        student = Student.create!(first_name: 'Dan', last_name: 'D', gender: 'm', gpa: 2.5, number_of_detentions: 8, sports_teams: ['Football', 'Foosball', 'Sand Hockey'], number_of_absences: 99, team_id: nil)
+        team = Team.create!(teacher_id: teacher.id, captain_id: student.id, team_name: 'Probably should have gone with name')
+        visit root_path
+
+        within('.login-form') do
+          fill_in :email, with: 'tmoney@ymail.com'
+          fill_in :password, with: 'password'
+          click_button('Login')
+        end
+
+        visit student_path(student)
+        expect(page).to have_current_path student_path(student)
+        expect(page).to have_selector(:link_or_button, "Add to Team")
+
       end
     end
 
     describe "when the student is already on the teacher's team" do
-      xscenario "the teacher sees a 'make new captain' button" do
+      scenario "the teacher sees a 'make new captain' button" do
+        teacher = Teacher.create!(first_name: 'Travis', last_name: 'CI', email: 'tmoney@ymail.com', password: 'password', admin?: false)
+        student = Student.create!(first_name: 'Dan', last_name: 'D', gender: 'm', gpa: 2.5, number_of_detentions: 8, sports_teams: ['Football', 'Foosball', 'Sand Hockey'], number_of_absences: 99, team_id: nil)
+        team = Team.create!(teacher_id: teacher.id, captain_id: nil, team_name: 'Probably should have gone with name')
+        student.team = team
+        student.save
+        visit root_path
+
+        within('.login-form') do
+          fill_in :email, with: 'tmoney@ymail.com'
+          fill_in :password, with: 'password'
+          click_button('Login')
+        end
+
+        visit student_path(student)
+        expect(page).to have_current_path student_path(student)
+        expect(page).to have_selector(:link_or_button, "Make Team Captain")
       end
     end
 
