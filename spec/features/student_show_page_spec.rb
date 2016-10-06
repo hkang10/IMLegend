a365e66cfc488d26754a4024ebdff3d5dca5b2c1
Dan Dobrick & Mark McLoughlin <pair+dandobrick.mark-mcl@devbootcamp.com>
2016-10-06 08:50:07 -0500

    write tests for student show

--- spec/features/student_show_page_spec.rb
+++ spec/features/student_show_page_spec.rb
@@ -4,21 +4,106 @@ feature "student's show page" do
   describe "when teacher navigates to a student's show page" do
 
     xscenario "the teacher sees the student's name" do
+      teacher = Teacher.create!(first_name: 'Travis', last_name: 'CI', email: 'tmoney@ymail.com', password: 'password', admin?: false)
+      student = Student.create!(first_name: 'Dan', last_name: 'D', gender: 'm', gpa: 2.5, number_of_detentions: 8, sports_teams: ['Football', 'Foosball', 'Sand Hockey'], number_of_absences: 99, team_id: nil)
+      team = Team.create!(teacher_id: teacher.id, captain_id: student.id, team_name: 'Probably should have gone with name')
+      visit root_path
+
+      within('.login-form') do
+        fill_in :email, with: 'tmoney@ymail.com'
+        fill_in :password, with: 'password'
+        click_button('Login')
+      end
+
+      visit student_path(student)
+      expect(page).to have_current_path student_path(student)
+      expect(page).to have_content student.first_name, student.last_name
     end
 
     xscenario "the teacher sees the student's gpa" do
+      teacher = Teacher.create!(first_name: 'Travis', last_name: 'CI', email: 'tmoney@ymail.com', password: 'password', admin?: false)
+      student = Student.create!(first_name: 'Dan', last_name: 'D', gender: 'm', gpa: 2.5, number_of_detentions: 8, sports_teams: ['Football', 'Foosball', 'Sand Hockey'], number_of_absences: 99, team_id: nil)
+      team = Team.create!(teacher_id: teacher.id, captain_id: student.id, team_name: 'Probably should have gone with name')
+      visit root_path
+
+      within('.login-form') do
+        fill_in :email, with: 'tmoney@ymail.com'
+        fill_in :password, with: 'password'
+        click_button('Login')
+      end
+
+      visit student_path(student)
+      expect(page).to have_current_path student_path(student)
+      expect(page).to have_content student.gpa
     end
 
     xscenario "the teacher sees the student's gender" do
+      teacher = Teacher.create!(first_name: 'Travis', last_name: 'CI', email: 'tmoney@ymail.com', password: 'password', admin?: false)
+      student = Student.create!(first_name: 'Dan', last_name: 'D', gender: 'm', gpa: 2.5, number_of_detentions: 8, sports_teams: ['Football', 'Foosball', 'Sand Hockey'], number_of_absences: 99, team_id: nil)
+      team = Team.create!(teacher_id: teacher.id, captain_id: student.id, team_name: 'Probably should have gone with name')
+      visit root_path
+
+      within('.login-form') do
+        fill_in :email, with: 'tmoney@ymail.com'
+        fill_in :password, with: 'password'
+        click_button('Login')
+      end
+
+      visit student_path(student)
+      expect(page).to have_current_path student_path(student)
+      expect(page).to have_content student.gender
     end
 
     xscenario "the teacher sees the student's number of detention" do
+      teacher = Teacher.create!(first_name: 'Travis', last_name: 'CI', email: 'tmoney@ymail.com', password: 'password', admin?: false)
+      student = Student.create!(first_name: 'Dan', last_name: 'D', gender: 'm', gpa: 2.5, number_of_detentions: 8, sports_teams: ['Football', 'Foosball', 'Sand Hockey'], number_of_absences: 99, team_id: nil)
+      team = Team.create!(teacher_id: teacher.id, captain_id: student.id, team_name: 'Probably should have gone with name')
+      visit root_path
+
+      within('.login-form') do
+        fill_in :email, with: 'tmoney@ymail.com'
+        fill_in :password, with: 'password'
+        click_button('Login')
+      end
+
+      visit student_path(student)
+      expect(page).to have_current_path student_path(student)
+      expect(page).to have_content student.number_of_detentions
     end
 
     xscenario "the teacher sees the student's sports team" do
+      teacher = Teacher.create!(first_name: 'Travis', last_name: 'CI', email: 'tmoney@ymail.com', password: 'password', admin?: false)
+      student = Student.create!(first_name: 'Dan', last_name: 'D', gender: 'm', gpa: 2.5, number_of_detentions: 8, sports_teams: ['Football', 'Foosball', 'Sand Hockey'], number_of_absences: 99, team_id: nil)
+      team = Team.create!(teacher_id: teacher.id, captain_id: student.id, team_name: 'Probably should have gone with name')
+      visit root_path
+
+      within('.login-form') do
+        fill_in :email, with: 'tmoney@ymail.com'
+        fill_in :password, with: 'password'
+        click_button('Login')
+      end
+
+      visit student_path(student)
+      expect(page).to have_current_path student_path(student)
+      student.sports_teams.each { |team| expect(page).to have_content team }
+
     end
 
-    xscenario "the teacher sees the student's number of detention" do
+    xscenario "the teacher sees the student's number of absences" do
+      teacher = Teacher.create!(first_name: 'Travis', last_name: 'CI', email: 'tmoney@ymail.com', password: 'password', admin?: false)
+      student = Student.create!(first_name: 'Dan', last_name: 'D', gender: 'm', gpa: 2.5, number_of_detentions: 8, sports_teams: ['Football', 'Foosball', 'Sand Hockey'], number_of_absences: 99, team_id: nil)
+      team = Team.create!(teacher_id: teacher.id, captain_id: student.id, team_name: 'Probably should have gone with name')
+      visit root_path
+
+      within('.login-form') do
+        fill_in :email, with: 'tmoney@ymail.com'
+        fill_in :password, with: 'password'
+        click_button('Login')
+      end
+
+      visit student_path(student)
+      expect(page).to have_current_path student_path(student)
+      expect(page).to have_content student.number_of_absences
     end
 
     describe "when the student is already on a team" do
