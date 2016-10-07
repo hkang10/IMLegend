bdf6e8b0fccef5bd346ea7158048bd855046f6c1
Dan Dobrick <dandobrick@gmail.com>
2016-10-07 10:58:47 -0500

    Add errors and validations for all models

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,7 +1,14 @@
 class Teacher < ActiveRecord::Base
   has_secure_password
-
   has_one :team
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :email, format: { with: /\S+@\S+\.\S/,
+    message: "must be a valid email address" }
+  validates :email, uniqueness: true
+
 
   def full_name
     self.first_name + ' ' + self.last_name
